0dfaa8a086a74fd09b024e3806ed94e053fa1648
jeasonchan <439994526@qq.com>
2019-05-19 23:09:12 +0800

    2019年5月19日23:09:08

--- src/default_package/ClassReflectExercise.java
+++ src/default_package/ClassReflectExercise.java
@@ -19,11 +19,74 @@ public class ClassReflectExercise {
         }
         System.out.println("Ieteger类的public构造方法数为："+arrayOfConstructor.length);
 
+        Class ClassForExample_01=Example_01.class;//新建Class类用于信息获取
+        Constructor[] declaredConstructorsOfExample_01=ClassForExample_01.getDeclaredConstructors();//获取所有的声明过的，包括私有、保护、公开构造方法
+        for(Constructor it:declaredConstructorsOfExample_01){//foreach遍历构造对象数组
+            Constructor instanceOfConstructor=it;//创建引用指向遍历的每一个对象，其实不是很必要……
+            System.out.println("当前构造方法是否允许可变数量参数："+instanceOfConstructor.isVarArgs());
+
+            System.out.println("当前构造方法的参数类型是：");
+            Class[] parameterTypes=instanceOfConstructor.getParameterTypes();//获取参数列表
+            for(Class itOfClass:parameterTypes){
+                System.out.println(itOfClass);
+            }
+
+            System.out.println("当前构造方法可能抛出的异常类型是：");//获取异常列表
+            Class[] instanceOfExceptions=it.getExceptionTypes();
+            for(Class itOfClass:instanceOfExceptions){
+                System.out.println(itOfClass);//java中一切皆对象，;可以说，Class类应该是和Object差不多的顶层类了
+            }
+
+        }
+
+        //通过反射得到的构造方法访问实例
+        Example_01 instanceOfExample=null;
 
 
 
+    }//end public static void runFunction()
 
+
+}
+
+
+class Example_01{
+    //用于反射练习
+    String s;
+    int i,i2,i3;
+
+    private Example_01(){
+        //构造函数
+    }
+
+    protected Example_01(String input_string,int intput_i){
+        //构造函数
+        this.s=input_string;
+        this.i=intput_i;
+    }
+
+    public Example_01(String...input_string)throws NumberFormatException{
+        //抛出异常说明，本方法不想/不能处理抛出的异常，要交给调用本方法的地方处理异常
+        if(input_string.length>0){
+            this.i=Integer.valueOf(input_string[0]);
+        }
+        if(input_string.length>1){
+            this.i2=Integer.valueOf(input_string[1]);
+        }
+        if(input_string.length>2){
+            this.i=Integer.valueOf(input_string[2]);
+        }
 
     }
+
+    public void print(){
+        System.out.println("s:"+this.s);
+        System.out.println("i:"+this.i);
+        System.out.println("i2:"+this.i2);
+        System.out.println("i3:"+this.i3);
+    }
+
+
+
 
 }
