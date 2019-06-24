031d058c3c034efbb643b13b6d2e28088165360b
jeasonchan <439994526@qq.com>
2019-06-25 00:47:47 +0800

    2019年6月25日00:47:28
    压缩包解压函数

--- src/main/java/default_package/ZipAndUnzipTool.java
+++ src/main/java/default_package/ZipAndUnzipTool.java
@@ -1,13 +1,90 @@
 package default_package;
 
-import java.io.File;
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
+
+import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ZipAndUnzipTool {
-    public static List<String> unTarFile(File file){
-        
+    public static List<String> unTarFile(File file) throws IOException {
+        List<String> fileNameList = new ArrayList<>();
+        String parentPath = file.getParent(); //得到父目录
+        TarArchiveInputStream tarArchiveInputStream = new TarArchiveInputStream(new FileInputStream(file));
 
+        TarArchiveEntry tarArchiveEntry = null;
 
+        while ((tarArchiveEntry = tarArchiveInputStream.getNextTarEntry()) != null) {
+            String name = tarArchiveEntry.getName();
+            fileNameList.add(name);
+            File targetDir = new File(parentPath + "\\" + name);  // 从将父目录下的文件条目新建一个文件对象
+            if (targetDir.getParentFile().exists()) {
+                targetDir.mkdirs();
+            }
+
+            BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream(targetDir));
+
+            int read = -1;
+            byte[] buffer = new byte[1024];
+            while ((read = tarArchiveInputStream.read(buffer)) != -1) {
+                bufferedOutputStream.write(buffer, 0, read); //将解压出的文件写入到缓冲流中
+            }
+            bufferedOutputStream.close();
+        }
+
+        tarArchiveInputStream.close();
+        return fileNameList;
     }
+
+    public static List<String> unTarFile2(File file) {
+        String basePath = file.getParent() + File.separator;
+        TarArchiveInputStream tarArchiveInputStream = null;
+        List<String> fileNamesList = new ArrayList<>();
+        TarArchiveEntry tarArchiveEntry = null;
+        try {
+            tarArchiveInputStream = new TarArchiveInputStream(new FileInputStream(file));
+            while ((tarArchiveEntry = tarArchiveInputStream.getNextTarEntry()) != null) {
+                fileNamesList.add(tarArchiveEntry.getName());
+                if (tarArchiveEntry.isDirectory()) {
+                    new File(basePath + tarArchiveEntry.getName()).mkdirs();
+                }
+                FileOutputStream fileOutputStream = null;
+                try {
+                    File f = new File(basePath + tarArchiveEntry.getName());
+                    if (!f.getParentFile().exists()) {
+                        f.getParentFile().mkdirs();
+                    }
+                    if (!f.exists()) {
+                        f.createNewFile();
+                    }
+                    fileOutputStream = new FileOutputStream(f);
+                    byte[] bs = new byte[2048];
+                    int len = -1;
+                    while ((len = tarArchiveInputStream.read(bs)) != -1) {
+                        fileOutputStream.write(bs, 0, len);
+                    }
+                    fileOutputStream.flush();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    fileOutputStream.close();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                tarArchiveInputStream.close();
+                // 解压后删除tar包
+                // file.delete();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        // 返回tar包下所有文件名
+        return fileNamesList;
+    }
+
 
 }
---
+++ src/test/java/default_package/ZipAndUnzipToolTest.java
@@ -0,0 +1,51 @@
+package default_package;
+
+import org.junit.Assert;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class ZipAndUnzipToolTest {
+    Comparator<String> comparator=new Comparator<String>() {
+        @Override
+        public int compare(String o1, String o2) {
+            return o1.compareToIgnoreCase(o2);
+        }
+    };
+
+
+    @org.junit.Test
+    public void unTarFileWithoutDir() throws IOException {
+        List<String> expectedList=new ArrayList<>();
+        expectedList.add("file1.txt");
+        expectedList.add("file2.txt");
+        expectedList.sort(comparator);
+        File file=new File("src\\test\\resources\\dir_without_dir.tar");
+
+        List<String> list=ZipAndUnzipTool.unTarFile2(file);
+        list.sort(comparator);
+
+        Assert.assertEquals(expectedList,list);
+
+    }
+
+    @org.junit.Test
+    public void unTarFileWithDir() throws IOException {
+        List<String> expectedList=new ArrayList<>();
+        expectedList.add("file1.txt");
+        expectedList.add("file2.txt");
+        expectedList.sort(comparator);
+        File file=new File("src\\test\\resources\\dir.tar");
+
+        List<String> list=ZipAndUnzipTool.unTarFile2(file);
+        list.sort(comparator);
+
+        Assert.assertEquals(expectedList,list);
+
+    }
+}
