6e0a7fa4bc984c1cb5b191ed21420420b4bf745e
jeasonchan <439994526@qq.com>
2019-11-06 23:47:02 +0800

    add optionalç»探索 package

---
+++ src/main/java/default_package/Optional探索/Main.java
@@ -0,0 +1,15 @@
+package default_package.Optional探索;
+
+
+import java.util.Optional;
+
+/**
+ * 探索一下的optional的用法
+ */
+
+
+public class Main {
+    public static void main(String[] args) {
+        Optional<String> stringOptional;
+    }
+}
