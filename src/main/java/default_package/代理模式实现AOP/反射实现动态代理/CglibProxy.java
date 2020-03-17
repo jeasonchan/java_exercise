315b3f32f3eac03b24734a80464cd9bab6c359d9
jeasonchan <439994526@qq.com>
2020-03-17 23:56:44 +0800

    update file

--- src/main/java/default_package/代理模式实现AOP/反射实现动态代理/CglibProxy.java
+++ src/main/java/default_package/代理模式实现AOP/反射实现动态代理/CglibProxy.java
@@ -1,13 +1,22 @@
 package default_package.代理模式实现AOP.反射实现动态代理;
 
 
+import net.sf.cglib.Enhancer;
 import net.sf.cglib.MethodInterceptor;
 import net.sf.cglib.MethodProxy;
 
+import java.awt.desktop.PreferencesHandler;
 import java.lang.reflect.Method;
 
 public class CglibProxy implements MethodInterceptor {
-    
+    private Object target;
+
+
+    public Object bind(Object target) {
+        //this.target = target;
+        //太麻烦，不做了……………………
+        return null;
+    }
 
 
     @Override
