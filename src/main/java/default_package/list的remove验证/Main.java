d88efd159493ce41782568d3437d96e8ea4b7db1
jeasonchan <439994526@qq.com>
2020-04-02 23:39:54 +0800

    验证remove的两种重载，涉及包装类的两种手动装箱、以及拆箱intValue

--- src/main/java/default_package/list的remove验证/Main.java
+++ src/main/java/default_package/list的remove验证/Main.java
@@ -1,4 +1,30 @@
 package default_package.list的remove验证;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        for (int index = 2; index < 12; index++) {
+            list.add(index);
+        }
+
+        System.out.println(list);//[2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+
+        //list.remove(Integer.valueOf(2));//[3, 4, 5, 6, 7, 8, 9, 10, 11]
+//        list.remove(2);//[2, 3, 5, 6, 7, 8, 9, 10, 11]
+        list.remove(new Integer(2));//[3, 4, 5, 6, 7, 8, 9, 10, 11]
+
+        /**
+         * ！！！！！！！！！！！！！！！！！
+         * 手动装箱的方式有两种！
+         * 注意 remove 两种重载，一种是的int入参的，按索引删除；
+         * 一种是传入对象，调用的对象的equals的方法作比较，然后删除
+         */
+
+        System.out.println(list);
+
+
+    }
 }
