934c9f165f155791a63bdacba8fedaeac2721876
jeasonchan <439994526@qq.com>
2020-04-15 08:49:59 +0800

    Update Main.java

--- src/main/java/default_package/按字典顺序输字符串排列组合/Main.java
+++ src/main/java/default_package/按字典顺序输字符串排列组合/Main.java
@@ -2,6 +2,6 @@ package default_package.按字典顺序输字符串排列组合;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(new Solution().getSortedList("haofcabciaia", Solution.ImplType.递归));
+        System.out.println(new Solution().getSortedList("abcdefg", Solution.ImplType.递推));
     }
 }
