a897b6327e7da5b3a63e26c4860d93bdd40f07d5
jeasonchan <439994526@qq.com>
2020-04-19 19:45:17 +0800

    finish function interface

--- src/main/java/default_package/函数式接口/GreetService.java
+++ src/main/java/default_package/函数式接口/GreetService.java
@@ -1,5 +1,6 @@
 package default_package.函数式接口;
 
+@FunctionalInterface
 public interface GreetService {
     void greet(String name);
 
--- src/main/java/default_package/函数式接口/ReturnGreeting.java
+++ src/main/java/default_package/函数式接口/ReturnGreeting.java
@@ -1,5 +1,6 @@
 package default_package.函数式接口;
 
+@FunctionalInterface
 public interface ReturnGreeting {
     String greeting(String name);
 }
