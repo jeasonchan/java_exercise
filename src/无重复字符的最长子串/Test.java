c8d637124475b30db68c293c50e76dee9852348c
jeasonchan <439994526@qq.com>
2020-04-21 15:34:03 +0800

    Create Test.java

---
+++ src/无重复字符的最长子串/Test.java
@@ -0,0 +1,7 @@
+package default_package.无重复字符的最长子串;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(new Solution().lengthOfLongestSubstring("dvdf"));
+    }
+}
