6906d4a3fa73818f28909bb385aab8db9c4ec147
jeasonchan <439994526@qq.com>
2020-04-21 08:48:07 +0800

    Create Solution.java

---
+++ 交错字符串/Solution.java
@@ -0,0 +1,30 @@
+package default_package.交错字符串;
+
+
+/*
+给定三个字符串s1 s2 s3，验证s3是否由s1 s2交错组成
+
+示例：
+输入：
+s1="aabcc"
+s2="dbbca"
+s3="aadbbcbcac"
+输出：true
+
+输入：
+s1="aabcc"
+s2="dbbca"
+s3="aadbbbaccc"
+输出：false
+
+ */
+public class Solution {
+    public boolean check(String s1, String s2, String target) {
+        
+
+
+
+        return false;
+    }
+
+}
