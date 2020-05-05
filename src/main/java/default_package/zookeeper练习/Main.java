0090789ab24ec538f59ec3cd8115b17659b880b7
jeasonchan <439994526@qq.com>
2020-05-05 20:12:25 +0800

    zookeeper增删改查实践

--- src/main/java/default_package/zookeeper练习/Main.java
+++ src/main/java/default_package/zookeeper练习/Main.java
@@ -1,6 +1,7 @@
 package default_package.zookeeper练习;
 
 import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.Stat;
 
 import java.io.IOException;
 import java.util.concurrent.CountDownLatch;
@@ -61,8 +62,54 @@ public class Main {
         String path = zooKeeper.create("/test01", "123456".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT_SEQUENTIAL);
         System.out.println("创建持久化有序号节点：" + path);
 
-        String path2 = zooKeeper.create("/test01", "123456".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
-        System.out.println("创建持久化无序号节点：" + path2);
+//        String path2 = zooKeeper.create("/test01", "123456".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+//        System.out.println("创建持久化无序号节点：" + path2);
+//        CreateMode.PERSISTENT 持久、无序号节点
+
+
+        //调用异步接口创造节点
+
+        //创建临时  有序号节点
+        CountDownLatch createdLatch = new CountDownLatch(1);
+
+        zooKeeper.create("/test", "233".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL,
+                new AsyncCallback.Create2Callback() {
+
+                    @Override
+                    public void processResult(int rc, String path, Object ctx, String name, Stat stat) {
+                        System.out.println("创建晚开始调用回调对象的processResult方法：");
+
+                        System.out.println(
+                                "rc:" + rc + "\n" +
+                                        "path:" + path + "\n" +
+                                        "ctx:" + ctx + "\n" +
+                                        "name:" + name + "\n" +
+                                        "stat:" + stat + "\n"
+                        );
+                        /*
+                        rc:0
+                        path:/test
+                        ctx:异步创建方法的ctx入参
+                        name:/test0000000006
+                        stat:58,58,1588679750798,1588679750798,0,0,0,72132398734704662,3,0,58
+
+                        其中，对于无序号的节点，path就是name，对于有序号的节点，name比path后面多十位数表示创建顺序
+                         */
+
+
+
+
+
+                        //使用CountDownLatch和匿名函数达到异步转同步
+                        createdLatch.countDown();
+                    }
+                }, "异步创建方法的ctx入参");
+
+
+        createdLatch.await();
+
+        //以同步的方式查询创建的临时节点的  父节点  下面的全部子节点  名称
+        System.out.println(zooKeeper.getChildren("/", false));
 
 
     }
