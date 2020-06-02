90cbeec70275e588d3db07099c93a62e9d3bd807
jeasonchan <439994526@qq.com>
2020-06-02 23:57:30 +0800

    增加java EE 的http的包到lib
    import javax.servlet.*;
    import javax.servlet.http.*;

--- src/main/java/default_package/mybatis练习/接口和实现类/Main.java
+++ src/main/java/default_package/mybatis练习/接口和实现类/Main.java
@@ -2,12 +2,14 @@ package default_package.mybatis练习.接口和实现类;
 
 import java.io.InputStream;
 
+import javax.servlet.*;
+import javax.servlet.http.*;
+
 public class Main {
     public static void main(String[] args) {
         String myBatisConfig = "mybatis2/config.xml";
 
         InputStream mybatisConfigStream = ClassLoader.getSystemResourceAsStream(myBatisConfig);
-
 
 
 
