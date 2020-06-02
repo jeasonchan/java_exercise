0eafd71aedc6f774db63a9a5acf537eaaa6f7483
jeasonchan <439994526@qq.com>
2020-06-03 00:45:39 +0800

    更新

--- src/main/java/default_package/mybatis练习/接口和实现类/Main.java
+++ src/main/java/default_package/mybatis练习/接口和实现类/Main.java
@@ -1,16 +1,22 @@
 package default_package.mybatis练习.接口和实现类;
 
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String myBatisConfig = "mybatis2/config.xml";
 
         InputStream mybatisConfigStream = ClassLoader.getSystemResourceAsStream(myBatisConfig);
 
+
+        ((BufferedInputStream) mybatisConfigStream).mark(0);
+        System.out.println(myBatisConfig + ":\n" + new String(mybatisConfigStream.readAllBytes()));
 
 
     }
