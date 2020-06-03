4a4911549c671aba9b75a305afbfa85ad06c54ea
jeasonchan <439994526@qq.com>
2020-06-04 00:37:11 +0800

    update

--- src/main/java/default_package/Pattern和Matcher/Main.java
+++ src/main/java/default_package/Pattern和Matcher/Main.java
@@ -1,5 +1,7 @@
 package default_package.Pattern和Matcher;
 
+import java.util.Arrays;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
@@ -32,6 +34,30 @@ public class Main {
                         "10"
                 )
         );
+
+
+        //=========================================
+        Pattern pattern_1 = Pattern.compile("<.*>");
+
+        String input = "<h1>jeason_chan</h1>";
+
+        Matcher matcher = pattern_1.matcher(input);
+        if (matcher.matches()) {
+            System.out.println(matcher.toMatchResult().start());
+            System.out.println(matcher.toMatchResult().end());
+
+        }
+
+        Pattern pattern_2 = Pattern.compile("<.*?>");
+        Matcher matcher2 = pattern_2.matcher(input);
+
+        if (matcher2.find()) {
+            System.out.println(matcher2.toMatchResult().start());
+            System.out.println(matcher2.toMatchResult().end());
+
+        }
+
+        System.out.println(Arrays.toString(pattern_2.split(input)));
 
     }
 }
